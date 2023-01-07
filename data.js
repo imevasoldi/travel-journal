9bb51f1e2fce24ba25b144829b80b72635849824
imevasoldi <imevasoldi@gmail.com>
2023-01-06 19:58:22 -0600

    add right pics and descriptions and links

--- data.js
+++ data.js
@@ -5,17 +5,8 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/cpx4wCdRtcT8GorY7",
         startDate: "16 Dec, 2022",
         endDate: "22 Jan, 2022",
-        description: "Juquila en el estado de Oaxaca es famoso por atraer turistas de todo el mundo. Cada año, en épocas decembrinas se puede disfrutar de su clima cálido en contraste con la temporada navideña.",
+        description: "El viajé comienza en medio de la sierra oaxaqueña,un largo camino lleno de curvas y hermosos paisajes nos lleva a Juquila un pequeño pueblo que esconde un hermoso tesoro,la virgen de Juquila, según los habitantes es muy muy milagrosa y se comprueba por la cantidad de peregrinos que ha diario llegan en bicicleta o incluso caminando, sin duda pasar por debajo de su manto es algo muy bonito, visitar el pedimento y observar atentos el simple y especial fervor de todo aquel que cruza ese sinuoso camino hasta la dulce virgen. Juquila es un lugar, cómo pocos quedan ya donde no encontrarás a  turismo extranjero o grandes cadenas hoteleras,ni mucho menos turistas fifis, solo gente sencilla con mucha fé.",
         imageUrl: "juquila.png"
-    },
-    {
-        title: "San Agustín, Oaxaca",
-        location: "México",
-        googleMapsUrl: "https://goo.gl/maps/FhcDbbM221aKE6CS9",
-        startDate: "16 Dec, 2022",
-        endDate: "22 Jan, 2022",
-        description: "En la bahía de San Agustín se puede disfrutar de nacer de las tortugas a la orilla del mar. Pertenece al distrito de Etla, dentro de la región valles centrales. Su cabecera es la localidad homónima.​",
-        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipPFTmOEHFL2S0BpXdVPIxXj8LtPQNkBlI0-oHBv=w408-h544-k-no"
     },
     {
         title: "Puerto Escondido, Oaxaca",
@@ -23,8 +14,26 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/KBNKBMSycGF2B2e9A",
         startDate: "16 Dec, 2022",
         endDate: "22 Jan, 2022",
-        description: 'Puerto Escondido es una ciudad portuaria y un balneario en la costa del Pacífico, en el estado mexicano de Oaxaca. Es conocida por sus numerosas playas y su animada vida nocturna. La playa Principal está rodeada de palmeras y bares con techos de paja. La ajetreada playa Zicatela es reconocida por la rompiente para el surf conocida como el "Pipeline mexicano". Cerca está la playa La Punta con olas más pequeñas. La playa Carrizalillo se ubica en una caleta resguardada por empinados acantilados.',
-        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipOjOnFcxg7wXdekEIAPox-7R06E0IUawFDph4DH=w533-h240-k-no"
+        description: 'Después de volver a recorrer ese laberinto de curvas para salir de Juquila llegamos a puerto escondido, sus hermosas playas nos esperaban,llegamos directo a playa manzanillo,lugar tranquilo y mar azul que te cautivará, no te dejes engañar por los lancheros no verás delfines ni ballenas a menos que contrates un tour a las 7:00 am. Lo que si es probable es ver una que otra tortuga indefensa atrapada por los lancheros. Ahí conocimos a Cecilia y su hermosa familia dueños de un restaurante de los muchos que hay en esa playa,los cuales te dan permiso de déjar tus cosas mientras te sanbulles en el agua, ellos nos ofrecieron un lugar tranquilo,sin mucha gente,cosa rara en estas épocas y nos trataron sumamente bien,la comida deliciosa y la plática bastante amena.',
+        imageUrl: "puerto-escondido.png"
+    },
+    {
+        title: "San Agustín, Oaxaca",
+        location: "México",
+        googleMapsUrl: "https://goo.gl/maps/XEjW3wnRyiTerDah9",
+        startDate: "16 Dec, 2022",
+        endDate: "22 Jan, 2022",
+        description: "Después de unas 4 horas de puerto escondido llegamos a San Agustín,he de confesar mi playa favorita en este viaje, lo primero que me enamoró fue la poca gente que había ( llegamos temprano así que nos tocó un buen rato de tranquilidad, después llegaron los turistas en lanchas y todo se llenó) lo segundo que me enamoró fue el color del agua  y lo tercero la fuerza de las olas,todo eso hizo de san Agustín una playa mágica,caminar por toda la orilla en tranquilidad y prácticamente para nosotras solas,con un único ruido,el sonido del mar fue una experiencia inolvidable. Ahí conocimos a Magda,mujer valiente que después de su jubilación decidío un buen día tomar su carro y viajar sola ,así a recorrido ya gran parte de México.",
+        imageUrl: "san-agustin.png"
+    },
+    {
+        title: "Huatulco, Oaxaca",
+        location: "México",
+        googleMapsUrl: "https://goo.gl/maps/mMBLtNWuMunUZoCR8",
+        startDate: "16 Dec, 2022",
+        endDate: "22 Jan, 2022",
+        description: "Si lo que quieras es una playa tranquila playa maguey es la opción,el agua es muy clara, las olas sumamente tranquilas y la arena es demasiado suave, en esta playa vivimos una hermosa experiencia,le dimos otra oportunidad a los lancheros y tomamos un tour para conocer algunas otras bahías de Huatulco, realmente valió le pena pues tuvimos la oportunidad de bajarnos en algunas playas virgenes con hermosísimos paisajes,nos maravillamos del poder del mar durante el recorrido en la lancha y conocimos a Bertha y su familia los cuales venían desde  otra costa en guerrero,ellos al igual que nosotros estaban enamorados de Huatulco y nos contaron cómo disfrutaron enormemente de estás playas incluso más que las playas de guerrero.",
+        imageUrl: "huatulco.png"
     },
     {
         title: "Oaxca, Oaxaca",
@@ -32,8 +41,8 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/8JbdovMhkEruBu626",
         startDate: "16 Dec, 2022",
         endDate: "22 Jan, 2022",
-        description: "Oaxaca es una ciudad del centro de México. Es conocida por sus edificios coloniales, muchos de los cuales están hechos de piedra volcánica verde. En la plaza central del Zócalo, se encuentra el Palacio de Gobierno, con coloridos murales que representan la historia regional. La plaza Alameda de León está frente a la fachada adornada de la Catedral de Oaxaca. La iglesia Templo de Santo Domingo del siglo XVI tiene un lujoso interior barroco y un convento antiguo a su lado.",
-        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipOgZQ5fdTEk1h-j6UnycWZKmdWAc-oA8MtCGirP=w408-h358-k-no"
+        description: "Nos despedimos de las playas oaxaqueñas y emprendimos nuestro viaje nocturno hacia la cuidad de Oaxaca, antes de llegar es imposible no pasar por los tradicionales lugares como las famosas pruebas de mezcal, cuyo principal fin es venderte sus productos; el árbol del tule,majestuoso ahuehuete que ha visto pasar la historia oaxaqueña, tomarnos un tejate fue indispensable para refrescarnos y una quesadilla de mole amarillo para darnos fuerza. Los talleres de textiles en Teotitlán nos hicieron recordar la magia que puede ocurrir en la artesanía Mexicana,lo comprobamos cuando conocimos a Don Florencio, maestro del telar que con mucha calma y amor por su arte nos explico cómo trabaja el telar,la facilidad con que sus manos hacen arte nos hizo reconocer su extraordinaria maestría.",
+        imageUrl: "oaxaca.png",
     },
     
 ]
