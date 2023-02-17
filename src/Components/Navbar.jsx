7476110f2eb9e98c926c9549b804b2d747b70c5d
imevasoldi <imevasoldi@gmail.com>
2023-02-16 20:20:59 -0600

    changing name in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar(){
     return (
         <nav>
             <img src='Fill 213.png'/>
-            <h1>J.M. Travel Journal</h1>
+            <h1>Juan J. Mejía Travel Journal</h1>
       </nav>
     )
 }
